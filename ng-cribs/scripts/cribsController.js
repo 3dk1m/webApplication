ae0ca8203f7cd3f3b2034efd49bcbf803b834097
Ed Kim <edk_23@hotmail.com>
2018-02-26 01:13:28 -0500

    Updates from lesson 7
    updates

--- ng-cribs/scripts/cribsController.js
+++ ng-cribs/scripts/cribsController.js
@@ -3,6 +3,31 @@ angular
     .controller('cribsController', function ($scope, cribsFactory) {
 
         $scope.hello = cribsFactory.getHelloWorld();
-        $scope.cribs = cribsFactory.getCribs();
+        $scope.cribs;
+
+        $scope.priceInfo = {
+            min: 0,
+            max: 1000000
+        }
+
+        cribsFactory.getCribs()
+            .then(function (data) {
+                $scope.cribs = data.data;
+                console.log($scope.cribs);
+            }, function (error) {
+                console.log(error)
+            });
+
+        $scope.sayHello = function () {
+            console.log("Hello");
+        }
+
+        /*         cribsFactory.getCribs()
+                    .then(function (data) {
+                        $scope.cribs = data;
+                        console.log($scope.cribs);
+                        if (!data) 
+                        console.log('error');
+                    }); */
 
     });
--- ng-cribs/scripts/cribsFactory.js
+++ ng-cribs/scripts/cribsFactory.js
@@ -1,31 +1,13 @@
 angular
     .module('ngCribs')
-    .factory('cribsFactory', function () {
+    .factory('cribsFactory', function ($http) {
 
         var helloWorld = 'Hello World!';
-        var cribsData = [
-            {
-                "type": "Condo",
-                "price": 220000,
-                "address": "213 Ceder Lane",
-                "description": "Excellent place, really nice view"
-            },
-            {
-                "type": "House",
-                "price": 410500,
-                "address": "783 Winding Way",
-                "description": "Excellent place, very large home"
-            },
-            {
-                "type": "Duplex",
-                "price": 395000,
-                "address": "834 Ceder Lane",
-                "description": "Excellent place, house is great for large family"
-            }
-        ];
+
 
         function _getCribs() {
-            return cribsData;
+            //mocking data with a json file under data folder.  usually this will have an endpoint where the data would come from
+            return $http.get('data/data.json');
         }
 
         function _getHelloWorld() {
